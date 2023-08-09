97a691693eb03d1ab60f6915695f3244bb1d5438
IAIR BARAK STEIMBERG <47569125@ort>
2023-08-09 15:54:17 -0300

    pregunta y opciones agragadas

--- ruleta/js/pregunta.js
+++ ruleta/js/pregunta.js
@@ -1,11 +1,13 @@
 function contador(objeto, tiempo, intervalo){
     window.setInterval(function(){
-        objeto.textContent = tiempo + "''";
-        tiempo--;
+        if(tiempo >= 0){
+            objeto.textContent = tiempo + "''";
+            tiempo--;
+        }
     }, intervalo * 1000);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     let objeto = document.getElementById("contador")
-    contador(objeto, 5, 1);
+    contador(objeto, 29, 1);
 })
